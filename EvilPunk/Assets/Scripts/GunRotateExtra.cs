0258bc423ec1b33e05c617840fc357161c5f550b
JonasDriessen0 <114576261+JonasDriessen0@users.noreply.github.com>
2023-06-13 13:09:44 +0200

    Core features further development

--- EvilPunk/Assets/Scripts/GunRotateExtra.cs
+++ EvilPunk/Assets/Scripts/GunRotateExtra.cs
@@ -2,16 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunRotate : MonoBehaviour
+public class GunRotateExtra : MonoBehaviour
 {
-    public Transform firePoint;
-    public float firePointFlipRot;
-    public GameObject bulletPrefab;
     private Transform m_transform;
     private float angle;
     private bool isFlipped = false;
-    private float nextbullet;
-    private float nextBULLETtime = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,19 +16,16 @@ public class GunRotate : MonoBehaviour
     // Update is called once per frame
     private void LAmouse()
     {
-        Vector2 center = new Vector2(Screen.width / 2, Screen.height / 2);
-        angle = Mathf.Atan2(Input.mousePosition.y - center.y, Input.mousePosition.x - center.x) * Mathf.Rad2Deg;
-        Debug.Log($"{angle} | {Input.mousePosition.x} | {Input.mousePosition.y}");
         transform.localEulerAngles = new Vector3(0, 0, angle);
         if (angle <= -90 || angle >= 90)
         {
             transform.GetComponent<SpriteRenderer>().flipY = true;
-            if(!isFlipped)
+            if (!isFlipped)
             {
                 isFlipped = true;
                 //firePoint.transform.localPosition = new Vector3(firePoint.transform.localPosition.x, firePoint.transform.localPosition.y - firePointFlipRot, firePoint.transform.localPosition.z);
             }
-            
+
         }
         else
         {
@@ -44,31 +36,10 @@ public class GunRotate : MonoBehaviour
                 //firePoint.transform.localPosition = new Vector3(firePoint.transform.localPosition.x, firePoint.transform.localPosition.y + firePointFlipRot, firePoint.transform.localPosition.z);
             }
         }
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Shoot();
-        }
-
-        if (nextbullet > 0)
-        {
-            nextbullet -= Time.deltaTime;
-        }
     }
 
     private void Update()
     {
         LAmouse();
-    }
-
-    void Shoot()
-    {
-        Debug.Log(nextbullet);
-        if(nextbullet >0)
-        {
-            return;
-        }
-        Instantiate(bulletPrefab, firePoint.position, Quaternion.Euler(90, 0, angle));
-        nextbullet = nextBULLETtime;
     }
 }
